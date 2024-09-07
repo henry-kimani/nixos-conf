faeb599ff37d61d0faf3c94579f9864cc5d32ec9
kim henry <kimanihenry14489@gmail.com>
2024-09-07 08:01:46 +0300

    modified the tailwind intellisence plugin

--- nvim/lua/hart/plugins/tailwindcss.lua
+++ nvim/lua/hart/plugins/tailwindcss.lua
@@ -1,12 +1,12 @@
 return {
-    --[[{
+    {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
                 tailwindcss = {},
             }
         }
-    }]]---
+    };
     {
         'NvChad/nvim-colorizer.lua',
         config = function ()
