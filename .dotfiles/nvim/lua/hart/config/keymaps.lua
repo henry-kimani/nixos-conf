44e5f5487605f566fea0d2b293eaeae10a4e9b5e
kim henry <kimanihenry14489@gmail.com>
2024-10-17 11:45:41 +0300

    Modified a nvim keymap

--- .dotfiles/nvim/lua/hart/config/keymaps.lua
+++ .dotfiles/nvim/lua/hart/config/keymaps.lua
@@ -5,7 +5,7 @@ k.set('n', '<leader>e', vim.cmd.Ex)
 k.set('n', '<C-s>', vim.cmd.w)
 k.set('n', '<C-`>', vim.cmd.terminal)
 
-k.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+k.set('n', '<leader>r', '<cmd>nohlsearch<CR>')
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', {}),
