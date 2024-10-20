876e3e4b25ad34047ea2296f40a3eb4c5ca17c51
kim henry <kimanihenry14489@gmail.com>
2024-10-20 20:27:24 +0300

    Added git aliases

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -4,6 +4,11 @@
     enable = true;
     userName = "kim henry";
     userEmail = "kimanihenry14489@gmail.com";
+    aliases = {
+      ct = "commit";
+      co = "checkout";
+      s = "status";
+    };
     extraConfig = {
       init.defaultBranch = "main";
     };
