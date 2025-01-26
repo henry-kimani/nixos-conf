89bc5f45e73c8ac395c587ab9f4780900b44c52f
kim henry <kimanihenry14489@gmail.com>
2025-01-27 00:00:04 +0300

    Changed the commit hash due to breaking changes or updating the plugin

--- .dotfiles/nvim/lua/hart/plugins/trouble.lua
+++ .dotfiles/nvim/lua/hart/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
     "folke/trouble.nvim",
+    commit = "4d36b8979287f5facc03fd6d955ace67db667e1d",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-    }
+    opts = {}
 }
