e78ccf437535837c48a55e914122fd2d3e8ad76e
kim henry <kimanihenry14489@gmail.com>
2025-10-30 23:23:24 +0300

    Redefined some keybinds

--- dotfiles/nvim/lua/hart/config/keymaps.lua
+++ dotfiles/nvim/lua/hart/config/keymaps.lua
@@ -1,14 +1,14 @@
-local k = vim.keymap
-
 vim.g.mapleader = " "
-k.set('n', '<leader>e', vim.cmd.Ex)
-k.set('n', '<C-s>', vim.cmd.w)
-k.set('i', 'jk', '<C-[>')
 
-k.set('n', '<leader>td', vim.cmd.Trouble)
-k.set('n', '<leader>tn', vim.cmd.tabnew)
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', '<C-s>', vim.cmd.w)
+vim.keymap.set('i', 'jk', '<C-[>')
 
-k.set('n', '<leader>r', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>td', vim.cmd.Trouble)
+vim.keymap.set('n', '<leader>tn', vim.cmd.tabnew)
+vim.keymap.set('n', '<leader>tc', vim.cmd.Tabclose)
+
+vim.keymap.set('n', '<leader>r', '<cmd>nohlsearch<CR>')
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', {}),
