f3dd04bf670601ac0c444fedf6c40274c379fcea
kim henry <kimanihenry14489@gmail.com>
2025-10-30 23:24:47 +0300

    Installed appflowy

--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -73,6 +73,7 @@
     pkgs.godot_4 # game engine
     pkgs.unityhub
     pkgs.discord
+    pkgs.appflowy # Notion alternative
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
